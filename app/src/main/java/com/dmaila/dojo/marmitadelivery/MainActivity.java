991145544f4852ebb5ee12b3902427b41e95b109
Daniele Maila Cebin <dmaila@ciandt.com>
2017-09-11 23:07:07 -0300

    Declara as views na MainActivity

--- app/src/main/java/com/dmaila/dojo/marmitadelivery/MainActivity.java
+++ app/src/main/java/com/dmaila/dojo/marmitadelivery/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dmaila.dojo.marmitadelivery;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText nameEditText = (EditText) findViewById(R.id.name);
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.meal_size_radiogroup);
+        EditText mainMealEditText = (EditText) findViewById(R.id.main_meal);
+        EditText sideDishEditText = (EditText) findViewById(R.id.side_dish);
+        EditText observationEditText = (EditText) findViewById(R.id.observation);
+        Button sendButton = (Button) findViewById(R.id.send_button);
+
+
     }
+
+
 }
+
+
+
