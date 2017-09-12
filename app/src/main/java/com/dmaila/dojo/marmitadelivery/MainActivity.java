a9562ffd8eb19afdeb1a24172bfcb160526223d1
Daniele Maila Cebin <dmaila@ciandt.com>
2017-09-11 23:28:01 -0300

    Exibe mensagem Toast ao tocar em enviar pedido

--- app/src/main/java/com/dmaila/dojo/marmitadelivery/MainActivity.java
+++ app/src/main/java/com/dmaila/dojo/marmitadelivery/MainActivity.java
@@ -3,6 +3,7 @@ package com.dmaila.dojo.marmitadelivery;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -11,17 +12,24 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    String name;
+    String mealSize;
+    String mainMeal;
+    String sideDish;
+    String observationText;
+    String orderToSend;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        EditText nameEditText = (EditText) findViewById(R.id.name);
+        final EditText nameEditText = (EditText) findViewById(R.id.name);
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.meal_size_radiogroup);
-        EditText mainMealEditText = (EditText) findViewById(R.id.main_meal);
-        EditText sideDishEditText = (EditText) findViewById(R.id.side_dish);
-        EditText observationEditText = (EditText) findViewById(R.id.observation);
+        final EditText mainMealEditText = (EditText) findViewById(R.id.main_meal);
+        final EditText sideDishEditText = (EditText) findViewById(R.id.side_dish);
+        final EditText observationEditText = (EditText) findViewById(R.id.observation);
         Button sendButton = (Button) findViewById(R.id.send_button);
 
 
@@ -29,8 +37,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, @IdRes int checkedID) {
                 RadioButton radioButton = (RadioButton) findViewById(checkedID);
+                mealSize = radioButton.getText().toString();
+//                Toast.makeText(MainActivity.this, radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
-                Toast.makeText(MainActivity.this, radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                name = nameEditText.getText().toString();
+                mainMeal = mainMealEditText.getText().toString();
+                sideDish = sideDishEditText.getText().toString();
+                observationText = observationEditText.getText().toString();
+
+                orderToSend = "Nome: " + name + "\n" +
+                        "Tamanho: " + mealSize + "\n" +
+                        "Mistura: " + mainMeal + "\n" +
+                        "Acompanhamento: " + sideDish + "\n" +
+                        "Observações: " + observationText;
+                Toast.makeText(MainActivity.this, orderToSend, Toast.LENGTH_SHORT).show();
+
             }
         });
 
