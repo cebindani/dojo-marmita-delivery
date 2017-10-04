8cf62156d19b82eca8b4b158b07933d1770612d7
Daniele Maila Cebin <dmaila@ciandt.com>
2017-10-04 13:01:22 -0300

    Adiciona botão Gerar recibo programaticamente

--- app/src/main/java/com/dmaila/dojo/marmitadelivery/MainActivity.java
+++ app/src/main/java/com/dmaila/dojo/marmitadelivery/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.root_view);
         final EditText nameEditText = (EditText) findViewById(R.id.name);
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.meal_size_radiogroup);
         final EditText mainMealEditText = (EditText) findViewById(R.id.main_meal);
@@ -46,14 +48,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        name = nameEditText.getText().toString();
+        mainMeal = mainMealEditText.getText().toString();
+        sideDish = sideDishEditText.getText().toString();
+        observationText = observationEditText.getText().toString();
+
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                name = nameEditText.getText().toString();
-                mainMeal = mainMealEditText.getText().toString();
-                sideDish = sideDishEditText.getText().toString();
-                observationText = observationEditText.getText().toString();
 
                 orderToSend = "Nome: " + name + "\n" +
                         "Tamanho: " + mealSize + "\n" +
@@ -69,12 +71,7 @@ public class MainActivity extends AppCompatActivity {
                         "&body=" + Uri.encode(orderToSend);
                 emailIntent.setData(Uri.parse(mailto));
 
-                //Abre tela de recibo
-                Intent voucherIntent = new Intent(MainActivity.this, VoucherActivity.class);
-                voucherIntent.putExtra("ORDER_SENT", orderToSend);
-
                 try {
-                    startActivity(voucherIntent);
                     startActivity(emailIntent);
                 } catch (ActivityNotFoundException e) {
                     Toast.makeText(MainActivity.this, "Não é possível enviar email!", Toast.LENGTH_SHORT).show();
@@ -84,6 +81,24 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        Button generateVoucherButton = new Button(this);
+        generateVoucherButton.setText("Gerar recibo");
+        generateVoucherButton.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        linearLayout.addView(generateVoucherButton);
+
+        generateVoucherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Abre tela de recibo
+                Intent voucherIntent = new Intent(MainActivity.this, VoucherActivity.class);
+                voucherIntent.putExtra("ORDER_SENT", orderToSend);
+
+                startActivity(voucherIntent);
+            }
+        });
     }
 
 
